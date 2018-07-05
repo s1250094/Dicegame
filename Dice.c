d3a735a38294262c3d0ec13b52fd19f73de58f24
s1250094 <s1250094@std3dc16>
2018-07-05 17:12:10 +0900

    Won/lost message added

--- Dice.c
+++ Dice.c
@@ -12,5 +12,10 @@ int main()
 
   printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d\n",d1,d2
 	 ,d1+d2);
+
+  if(d1+d2 > 7)
+    printf("You won\n");
+  else printf("You lost\n");
+  
   return 0;
 }
